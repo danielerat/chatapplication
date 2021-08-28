756f8f5f6f5018baaf49e3802df2d00470c52760
evalisteT <87124670+evalisteT@users.noreply.github.com>
2021-08-28 12:19:31 +0200

    Update database.php
    changing credentials of the database

--- private/database.php
+++ private/database.php
@@ -1,8 +1,8 @@
 <?php
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "danielerat");
-define("DB_PASS", "dbpassword");
+define("DB_USER", "root");
+define("DB_PASS", "");
 define("DB_NAME", "chatapplication");
 
 //Creating and Confirming Database Connection
